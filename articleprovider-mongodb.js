0522403fdf56fc35c07faf6d1a5bb367f8d7bfe4
domnguyen87 <domnguyen@gmail.com>
2019-08-19 15:37:13 -0700

    Update articleprovider-mongodb.js

--- articleprovider-mongodb.js
+++ articleprovider-mongodb.js
@@ -16,3 +16,28 @@ ArticleProvider.prototype.getCollection= function(callback) {
     else callback(null, article_collection);
   });
 };
+
+ArticleProvider.prototype.findAll = function(callback) {
+    this.getCollection(function(error, article_collection) {
+      if( error ) callback(error)
+      else {
+        article_collection.find().toArray(function(error, results) {
+          if( error ) callback(error)
+          else callback(null, results)
+        });
+      }
+    });
+};
+
+
+ArticleProvider.prototype.findById = function(id, callback) {
+    this.getCollection(function(error, article_collection) {
+      if( error ) callback(error)
+      else {
+        article_collection.findOne({_id: article_collection.db.bson_serializer.ObjectID.createFromHexString(id)}, function(error, result) {
+          if( error ) callback(error)
+          else callback(null, result)
+        });
+      }
+    });
+};
